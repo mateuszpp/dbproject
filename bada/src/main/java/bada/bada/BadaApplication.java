a233ac041fd2ed495618d6551f633658bfc2c3b3
mplicht1 <01178300@pw.edu.pl>
2023-12-27 16:11:57 +0100

    bada_cz2_after 3 presentations

---
+++ bada/src/main/java/bada/bada/BadaApplication.java
@@ -0,0 +1,13 @@
+package bada.bada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaApplication.class, args);
+	}
+
+}
---
+++ bada/src/test/java/bada/bada/BadaApplicationTests.java
@@ -0,0 +1,13 @@
+package bada.bada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
